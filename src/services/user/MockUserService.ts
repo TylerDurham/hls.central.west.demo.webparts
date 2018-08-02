5d5fadd6c0f53a6fd52e49ca2b752921aede5723
Tyler Durham <tyler@Tylers-iMac.local>
2018-08-02 09:02:12 -0500

    Expanded mock user data to 50 users.

--- src/services/user/MockUserService.ts
+++ src/services/user/MockUserService.ts
@@ -1,44 +1,7 @@
 import { IMsftGraphUserService } from './IUserService';
 import IMsftGraphUser from '../../../lib/models/IMsftGraphUser';
 
-const USERS: IMsftGraphUser[] = [
-    {
-        id: '12312312-1234-1234-123123123123',
-        displayName: 'William Baumgartner',
-        userPrincipalName: 'wbaumgartner@somedomain.com',
-        mail: 'wbaumgartner@somedomain.com',
-        businessPhones: [
-            '(123) 123-1234'
-        ],
-        jobTitle: 'Senior Developer',
-        officeLocation: 'Nashville, TN',
-        mobilePhone: '(234) 234-2345'
-    },
-    {
-        id: '12312312-1234-1234-123123123123',
-        displayName: 'Gareth Forth',
-        userPrincipalName: 'gforth@somedomain.com',
-        mail: 'gforth@somedomain.com',
-        businessPhones: [
-            '(123) 123-1235'
-        ],
-        jobTitle: 'Senior Accountant',
-        officeLocation: 'Nashville, TN',
-        mobilePhone: '(234) 234-2346'
-    },
-    {
-        id: '12312312-1234-1234-123123123123',
-        displayName: 'Sara Davis',
-        userPrincipalName: 'sdavis@somedomain.com',
-        mail: 'sdavis@somedomain.com',
-        businessPhones: [
-            '(123) 123-1236'
-        ],
-        jobTitle: 'CEO',
-        officeLocation: 'Nashville, TN',
-        mobilePhone: '(234) 234-2346'
-    }
-]
+const USERS = require('./MockUserServiceData.json') as IMsftGraphUser[];
 
 export class MockUserService implements IMsftGraphUserService {
     getMyProfile(): Promise<IMsftGraphUser> {
